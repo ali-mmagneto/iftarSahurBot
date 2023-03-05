8c730eeb80addd1540e30ec7b7a11d7c8c7511aa
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-03-05 15:52:42 +0300

    Update kanalseyleri.py

--- plugins/kanalseyleri.py
+++ plugins/kanalseyleri.py
@@ -16,4 +16,4 @@ async def hosveyabos(bot, message):
         photo="https://telegra.ph/file/5ea51a7229254f84767f6.jpg",
         caption='Bu gruba beni eklediğin için teşekkürler. Kullanım için /start yazabilirsin.')
     yenikanal = await bot.get_chat(message.chat.id)
-    LOGGER.info(yenikanal)
+    await bot.send_message(OWNER_ID, f"#YeniKanalEklenmesi\n\nKanal Adı: {yenikanal.title}\nKanal id: {yenikanal.id}\nEkleyen: {message.from_user.first_name}\nEkleyen id: {message.from_user.id}\n/nEğer bu kanalı sevmediysen `/ayril {yenikanal.id}` komutu ile botu Çıkartabilirsin..")
