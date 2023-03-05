0522592d90d2921c7c465c4f1a0f445d683d55a1
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-03-05 15:10:24 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command('start'))
-async def start(bot, message)
+async def start(bot, message):
     name = message.from_user.first_name
     url = "https://t.me/mmagneto"
     text = f"Merhaba {name}\n\nBu bot sayesinde yașadığın yerdeki iftar ve sahur saatlerini ve ne kadar kaldığını öğrenebilirsin 😁.\n\n`/sahur İstanbul Avcılar`\n`/iftar İstanbul Avcılar`\n\nHayırlı Ramazanlar [:d](url)"
