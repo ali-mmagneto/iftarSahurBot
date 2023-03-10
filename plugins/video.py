31cfe4419d7a5c658ea6968087e34dafa9622a0a
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-03-10 23:23:47 +0300

    Update video.py

--- plugins/video.py
+++ plugins/video.py
@@ -3,7 +3,7 @@ import os, youtube_dl, requests, time
 from youtube_search import YoutubeSearch
 import asyncio
 
-async def indir(link, ydl_opts)
+async def indir(link, ydl_opts):
     download_directory = "downloads/ytvideo.mp4"
     command_to_exec = [
         "yt-dlp",
