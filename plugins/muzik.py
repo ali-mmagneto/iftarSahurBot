a5604272cab5a9ded68ad9b114a16faf480696cc
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-03-11 22:45:29 +0300

    Update muzik.py

--- plugins/muzik.py
+++ plugins/muzik.py
@@ -87,7 +87,7 @@ async def playlist(bot, message):
                 for i in range(len(dur)-1, -1, -1):
                     durationn += (int(dur[i]) * carp)
                     carp *= 60
-                await m.edit("`{title} Yükleniyor..`")
+                await m.edit(f"`{title} Yükleniyor..`")
                 await bot.send_audio(
                     chat_id=message.chat.id,
                     audio=audio_file, 
