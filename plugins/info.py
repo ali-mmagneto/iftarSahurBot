7ae0e7d25623c7dd34f0e9625f4262f18ee21999
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-03-12 13:42:57 +0300

    Update info.py

--- plugins/info.py
+++ plugins/info.py
@@ -12,6 +12,7 @@ async def info(bot, message):
             text = await bot.get_chat(message.chat.id)
             await message.reply_text(text)
             async for photo in bot.get_chat_photos(message.chat.id, limit=1):
+                LOGGER.info(photo)
                 await message.reply_photo(photo) 
         else:
             text = message.from_user
