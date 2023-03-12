8aa012b0c4ef6ea1130f0e1b91b3ec209d39a98d
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-03-12 12:27:11 +0300

    Create info.py

---
+++ plugins/info.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+
+
+@Client.on_message(filters.command('info'))
+async def info(bot, message):
+    try:
+        if not message.reply_to_message:
+            text = await bot.get_chat(message.chat.id)
+            await message.reply_text(text)
+        else:
+            text = message.from_user
+            await message.reply_text(text)
+     except Exception as e:
+         await message.reply_text(e)
