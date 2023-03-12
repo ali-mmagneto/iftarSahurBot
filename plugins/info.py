ac687ceb057183104815fc86b0d23088d18d7407
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-03-12 14:44:40 +0300

    Update info.py

--- plugins/info.py
+++ plugins/info.py
@@ -13,14 +13,18 @@ async def info(bot, message):
             await message.reply_text(text)
             async for photo in bot.get_chat_photos(message.chat.id, limit=1):
                 LOGGER.info(photo)
-                caption = f"**Grup Adı**: {text.title}\n**Grup Id**: `{text.id}`\n\n**Senin Adın**: {message.from_user.first_name}\n**Senin Kullanıcı Adı**: @{message.from_username}**Senin Id**: `{message.from_user.id}`\n**Senin Dc**: {message.from_user.dc_id}\n**Sen**: {message.from_user.mention}"
+                caption = f"**Grup Adı**: {text.title}\n**Grup Id**: `{text.id}`\n\n**Senin Adın**: {message.from_user.first_name}\n**Senin Kullanıcı Adı**: @{message.from_user.username}**Senin Id**: `{message.from_user.id}`\n**Senin Dc**: {message.from_user.dc_id}\n**Sen**: {message.from_user.mention}"
                 await message.reply_photo(photo.file_id, caption=caption) 
         else:
             text = message.from_user
             await message.reply_text(text)
+            if text.is_premium == false:
+                pre = "Normal Üye"
+            else:
+                pre = "Premium Üye" 
             async for photo in bot.get_chat_photos(message.reply_to_message.from_user.id, limit=1):
                 LOGGER.info(photo)
-                caption = f"**Adı**: {message.reply_to_message.from_user.first_name}\n**Kullanıcı Adı**: @{message.reply_to_message.from_user.username}\n**Id**: `{message.reply_to_message.from_user.id}`\n**Dc Id**: {message.reply_to_message.from_user.dc_id}\n**O**: {message.reply_to_message.from_user.mention}"
+                caption = f"**Adı**: {message.reply_to_message.from_user.first_name}\n**Durum**: {pre}\n**Kullanıcı Adı**: @{message.reply_to_message.from_user.username}\n**Id**: `{message.reply_to_message.from_user.id}`\n**Dc Id**: {message.reply_to_message.from_user.dc_id}\n**O**: {message.reply_to_message.from_user.mention}"
                 await message.reply_photo(photo.file_id, caption=caption) 
     except Exception as e:
         await message.reply_text(e)
