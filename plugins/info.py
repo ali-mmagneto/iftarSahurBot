2f00b5ff2003579d6edffddd449e3f0237e9480d
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-03-12 13:55:17 +0300

    Update info.py

--- plugins/info.py
+++ plugins/info.py
@@ -13,10 +13,13 @@ async def info(bot, message):
             await message.reply_text(text)
             async for photo in bot.get_chat_photos(message.chat.id, limit=1):
                 LOGGER.info(photo)
-                caption = f"Grup Adı: {text.title}\nGrup Id: {text.id}\n\nSenin Adın:{message.from_user.first_name}\nSenin Id: {message.from_user.id}\nSen: {message.from_user.mention}"
+                caption = f"Grup Adı: {text.title}\nGrup Id: {text.id}\n\nSenin Adın: {message.from_user.first_name}\nSenin Id: {message.from_user.id}\nSen: {message.from_user.mention}"
                 await message.reply_photo(photo.file_id, caption=caption) 
         else:
             text = message.from_user
-            await message.reply_text(text)
+            async for photo in bot.get_chat_photos(message.reply_to_message.from_user.id, limit=1):
+                LOGGER.info(photo)
+                caption = f"Adı: {message.reply_to_message.from_user.first_name}\nId: {message.reply_to_message.from_user.id}\nDc Id: {message.reply_to_message.from_user.dc_id}O: {message.reply_to_message.from_user.mention}"
+                await message.reply_photo(photo.file_id, caption=caption) 
     except Exception as e:
         await message.reply_text(e)
