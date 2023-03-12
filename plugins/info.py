47a1f11c1523dfaaecb89a60216e841c61990c66
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-03-12 13:55:58 +0300

    Update info.py

--- plugins/info.py
+++ plugins/info.py
@@ -5,7 +5,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
-@Client.on_message(filters.command('info'))
+@Client.on_message(filters.command('bilgi'))
 async def info(bot, message):
     try:
         if not message.reply_to_message:
